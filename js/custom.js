e28a58d03dc469aa6d364f83160c0a9a3dd36190
sajedul-dev <sajedulislamraja@gmail.com>
2023-06-25 15:31:55 +0600

    first git push purpose

---
+++ js/custom.js
@@ -0,0 +1,56 @@
+$(function(){
+
+  //slick slider
+    
+  $('.review-wrapper').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows:false,
+        centerMode: true,
+        dots:true,
+      })
+
+  //counter up
+
+      $('.counter').counterUp({
+        delay: 100,
+        time: 2000
+  });
+
+  //bottom to top
+
+    $(".bottom-to-top").click(function(){
+      $("body,html") .animate({scrollTop:0},1000)
+    })
+    $(window).scroll(function(){
+      let scroll = $(this).scrollTop()
+      console.log(scroll)
+      if(scroll > 300 ){
+        $(".bottom-to-top").fadeIn(500)
+      }else{
+        $(".bottom-to-top").fadeOut(500)
+      }
+      //nav fixed operation
+      
+      if( scroll > 130){
+        $(".nav-main").addClass("fixed")
+      }else{
+        $(".nav-main").removeClass("fixed")
+      }
+    })
+
+    //preloader start
+
+    $(window).on('load',function(){
+      $(".preloader-main").delay(1000).fadeOut(500)
+    })
+
+    //mixitup plugin
+
+    $('.all').on('click',function(){
+      $(this).addClass('active').siblings().removeClass('active')
+    })
+
+})
+
